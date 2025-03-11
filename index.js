12aa1387c5a4d2ede4b9a42f2f3b0a6b79812693
miss australiana <git@missaustraliana.net>
2025-03-11 15:50:34 +1100

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const { createServer } = require('node:http');
-
-const app = express();
-const server = createServer(app);
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+  // ...
 });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+io.on("connection", (socket) => {
+  // ...
 });
+
+httpServer.listen(3000);
