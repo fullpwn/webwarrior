558e3e6a3e709b8ccbc3c22b7a77fd808510a081
miss australiana <git@missaustraliana.net>
2025-03-11 16:22:09 +1100

    create 2 files and update 2 files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
-
+const { exec } = require('child_process');
 // Initialize Express app
 const app = express();
 const server = http.createServer(app);
@@ -15,6 +15,7 @@ app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
 
 // Socket.IO connection handler
 io.on('connection', (socket) => {
@@ -40,7 +41,18 @@ io.on('connection', (socket) => {
     io.emit('message', `User ${socket.id} has disconnected`);
   });
 });
+function sendTime() {
+  exec('date +%s.%N', (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    io.emit('lunch', { time: stdout })
+  }
+)}
 
+// Send current time every 10 secs
+setInterval(sendTime, 100);
 // Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
